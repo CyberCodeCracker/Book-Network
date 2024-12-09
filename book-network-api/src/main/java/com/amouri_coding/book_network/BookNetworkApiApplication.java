1621618ffc04411278ac298309a3105cd0653640
CyberCodeCracker <souhailamouri123@gmail.com>
2024-12-09 10:03:47 +0100

    Added ApplicationAuditAware class and it's functionality

--- book-network-api/src/main/java/com/amouri_coding/book_network/BookNetworkApiApplication.java
+++ book-network-api/src/main/java/com/amouri_coding/book_network/BookNetworkApiApplication.java
@@ -7,12 +7,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableJpaRepositories
-@EnableJpaAuditing
+@EnableJpaAuditing(auditorAwareRef = "auditorAware ")
 @EnableAsync
 public class BookNetworkApiApplication {
 
