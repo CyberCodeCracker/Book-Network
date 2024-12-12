f132359800c3aedc7de46cb10335438a93f8ab6e
CyberCodeCracker <souhailamouri123@gmail.com>
2024-12-12 15:57:20 +0100

    Implemented findAllBooks method

--- book-network-api/src/main/java/com/amouri_coding/book_network/book/BookController.java
+++ book-network-api/src/main/java/com/amouri_coding/book_network/book/BookController.java
@@ -1,5 +1,6 @@
 package com.amouri_coding.book_network.book;
 
+import com.amouri_coding.book_network.common.PageResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -31,5 +32,11 @@ public class BookController {
     }
 
     @GetMapping
-    public ResponseEntity<List>
+    public ResponseEntity<PageResponse<BookResponse>> findAllBooks(
+            @RequestParam(name = "page", defaultValue = "0", required = false) int page,
+            @RequestParam(name = "size", defaultValue = "10", required = false) int size,
+            Authentication connectedUser
+    ) {
+        return ResponseEntity.ok(service.findAllBooks(page, size, connectedUser));
+    }
 }
--- book-network-api/src/main/java/com/amouri_coding/book_network/book/BookService.java
+++ book-network-api/src/main/java/com/amouri_coding/book_network/book/BookService.java
@@ -1,10 +1,18 @@
 package com.amouri_coding.book_network.book;
 
+import com.amouri_coding.book_network.common.PageResponse;
 import com.amouri_coding.book_network.user.User;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -23,6 +31,25 @@ public class BookService {
     public BookResponse findById(Integer bookId) {
         return bookRepository.findById(bookId)
                 .map(bookMapper::toBookResponse)
-                .orElseThrow(() -> EntityNotFoundException("Book associated with " + bookId + " was not found."));
+                .orElseThrow(() -> new EntityNotFoundException("Book associated with " + bookId + " was not found."));
+    }
+
+    public PageResponse<BookResponse> findAllBooks(int page, int size, Authentication connectedUser) {
+        User user = ((User) connectedUser.getPrincipal());
+        Pageable pageable = PageRequest.of(page, size, Sort.by("createdDate").descending());
+        Page<Book> books = bookRepository.findAllDisplayableBooks(pageable, user.getId());
+        List<BookResponse> bookResponses = books.stream()
+                .map(bookMapper::toBookResponse)
+                .toList()
+                ;
+        return new PageResponse<>(
+                bookResponses,
+                books.getNumber(),
+                books.getSize(),
+                books.getTotalElements(),
+                books.getTotalPages(),
+                books.isFirst(),
+                books.isLast()
+        );
     }
 }
