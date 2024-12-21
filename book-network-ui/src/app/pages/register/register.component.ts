61756ccf22b5d8c46a5c35ab6afebd827915e790
CyberCodeCracker <souhailamouri123@gmail.com>
2024-12-21 10:07:56 +0100

    Added login page and functionality

---
+++ book-network-ui/src/app/pages/register/register.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {RegistrationRequest} from "../../services/models/registration-request";
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent {
+
+
+  registerRequest: RegistrationRequest = {email: '', firstName: '', lastName: '', password: ''};
+  errorMsg: Array<string> = [];
+
+}
