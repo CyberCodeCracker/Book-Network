63e4390f4c6e5ddbb2814c5555e5c65ada54a90d
CyberCodeCracker <souhailamouri123@gmail.com>
2024-12-23 19:25:21 +0100

    Added book card component

---
+++ book-network-ui/src/app/modules/book/components/book-card/book-card.component.ts
@@ -0,0 +1,70 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {BookResponse} from "../../../../services/models/book-response";
+
+@Component({
+  selector: 'app-book-card',
+  templateUrl: './book-card.component.html',
+  styleUrls: ['./book-card.component.scss']
+})
+export class BookCardComponent {
+
+  private _book: BookResponse = {};
+  private _manage: boolean = false;
+  private _bookCover: string | undefined;   // cover is not mandatory
+
+  get book(): BookResponse {
+    return this._book;
+  }
+
+  @Input()
+  set book(value: BookResponse) {
+    this._book = value;
+  }
+
+  get manage(): boolean {
+    return this._manage;
+  }
+
+  @Input()
+  set manage(value: boolean) {
+    this._manage = value;
+  }
+
+  get bookCover(): string | undefined {
+    if (this._book.bookCover) {
+      return 'data:image/jpg;base64, ' + this._book.bookCover;
+    }
+    return this._bookCover;
+  }
+
+  @Output() private showDetails: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+  @Output() private borrow: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+  @Output() private addToWaitingList: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+  @Output() private edit: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+  @Output() private share: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+  @Output() private archive: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+
+  onShowDetails() {
+    this.showDetails.emit(this._book);
+  }
+
+  onBorrow() {
+    this.borrow.emit(this._book);
+  }
+
+  onAddToWaitingList() {
+    this.addToWaitingList.emit(this._book);
+  }
+
+  onEdit() {
+    this.edit.emit(this._book);
+  }
+
+  onShare() {
+    this.share.emit(this._book);
+  }
+
+  onArchive() {
+    this.archive.emit(this._book);
+  }
+}
