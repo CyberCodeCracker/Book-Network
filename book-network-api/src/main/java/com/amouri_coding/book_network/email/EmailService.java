ec46303ff892eb0aa62ba03fd994130d53376c69
CyberCodeCracker <souhailamouri123@gmail.com>
2025-01-26 09:53:23 +0100

    Added gmail config for mail service

--- book-network-api/src/main/java/com/amouri_coding/book_network/email/EmailService.java
+++ book-network-api/src/main/java/com/amouri_coding/book_network/email/EmailService.java
@@ -3,6 +3,7 @@ package com.amouri_coding.book_network.email;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
@@ -10,14 +11,16 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 
-import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.springframework.mail.javamail.MimeMessageHelper.MULTIPART_MODE_MIXED;
+
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class EmailService {
-
     private final JavaMailSender mailSender;
     private final SpringTemplateEngine templateEngine;
 
@@ -31,33 +34,33 @@ public class EmailService {
             String subject
     ) throws MessagingException {
         String templateName;
-        if(emailTemplate == null) {
+        if (emailTemplate == null) {
             templateName = "confirm-email";
         } else {
             templateName = emailTemplate.getName();
-            MimeMessage mimeMessage = mailSender.createMimeMessage();
-            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(
-                    mimeMessage,
-                    MimeMessageHelper.MULTIPART_MODE_MIXED,
-                    StandardCharsets.UTF_8.name()
-            );
-            Map<String, Object> properties = new HashMap<>();
-            properties.put("username", username);
-            properties.put("confirmationUrl", confirmationUrl);
-            properties.put("activationCode", activationCode);
-
-            Context context = new Context();
-            context.setVariables(properties);
-
-            mimeMessageHelper.setFrom("contact@amouricoding.com");
-            mimeMessageHelper.setTo(to);
-            mimeMessageHelper.setSubject(subject);
-
-            String template = templateEngine.process(templateName, context);
-
-            mimeMessageHelper.setText(template, true);
-
-            mailSender.send(mimeMessage);
         }
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(
+                mimeMessage,
+                MULTIPART_MODE_MIXED,
+                UTF_8.name()
+        );
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("username", username);
+        properties.put("confirmationUrl", confirmationUrl);
+        properties.put("activation_code", activationCode);
+
+        Context context = new Context();
+        context.setVariables(properties);
+
+        helper.setFrom("contact@aliboucoding.com");
+        helper.setTo(to);
+        helper.setSubject(subject);
+
+        String template = templateEngine.process(templateName, context);
+
+        helper.setText(template, true);
+
+        mailSender.send(mimeMessage);
     }
 }
--- book-network-api/src/main/java/com/amouri_coding/book_network/email/EmailTemplateName.java
+++ book-network-api/src/main/java/com/amouri_coding/book_network/email/EmailTemplateName.java
@@ -4,11 +4,13 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public enum EmailTemplateName {
 
     ACTIVATE_ACCOUNT("activate_account")
 
     ;
     private final String name;
+    EmailTemplateName(String name) {
+        this.name = name;
+    }
 }
